4982f2879a891e1a393fd5b7262896eb1cc7a76b
Dave Bray <david@brayworth.com.au>
2021-02-05 11:18:06 +1000

    forum 7023 - Extra Columns

--- src/smokealarm/dao/smokealarm.php
+++ src/smokealarm/dao/smokealarm.php
@@ -285,8 +285,8 @@ class smokealarm extends _dao {
 				$res->dtoSet( function( $dto) use ( $leaseDetails) {
 					$key = array_search( $dto->properties_id, \array_column( $leaseDetails, 'property_id'));
 					if ( $key !== false) {
-						if ( strtotime( $leaseDetails[$key]->leaseStop) > 0) {
-							\sys::logger( sprintf('<%s> %s', $leaseDetails[$key]->leaseStop, __METHOD__));
+						if ( strtotime( $leaseDetails[$key]->LeaseStop) > 0) {
+							\sys::logger( sprintf('<%s> %s', $leaseDetails[$key]->LeaseStop, __METHOD__));
 
 						}
 
