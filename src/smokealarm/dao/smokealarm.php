5f9a6a3f390b4ebb695eb9858c393238fb502eed
Dave Bray <david@brayworth.com.au>
2021-02-08 11:58:55 +1000

    forum 7045 -  duplicate properties

--- src/smokealarm/dao/smokealarm.php
+++ src/smokealarm/dao/smokealarm.php
@@ -345,7 +345,7 @@ class smokealarm extends _dao {
 		// $this->Q('DROP TABLE IF EXISTS _smokealarm_tmp');
 		// $this->Q('CREATE TABLE IF NOT EXISTS _smokealarm_tmp SELECT * FROM tmp');
 
-		return $this->Result( 'SELECT * FROM tmp ORDER BY `smokealarms_last_inspection`, `peoperties_id` LIMIT 3000');
+		return $this->Result( 'SELECT * FROM tmp ORDER BY `smokealarms_last_inspection`, `properties_id` LIMIT 3000');
 
 	}
 
